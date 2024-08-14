b5964c94e5908a49e9d8da30309a94bf5ceb1e93
Angel Jimenez <ajimenezcruz184@gmail.com>
2024-08-14 10:30:53 -0600

    determinar si cada numero de la serie es primo.

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -9,10 +9,20 @@ namespace miPrimerProyecto
     internal class Program
 {
     static void Main(string[] args)
+            //determinar si cada numero de la serie es primo.
     {
-        int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+        int[] serie = new int[] { 5, 9, 4, 6, 3, 2,7,11,15,17,21 };
         foreach (int num in serie) {
-            Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "par" : "impar");
+                int i = 1,
+                    n = 0;
+                while (i <= num && n <= 2) { 
+                
+                    if (num % i  == 0){
+                        n++;
+                    }
+                    i++;
+                }
+                Console.WriteLine("el numero {0} {1} ", num, n <= 2 ? "es primo" : "no es primo");
         }
         Console.ReadLine();
     }
