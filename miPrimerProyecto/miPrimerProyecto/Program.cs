747e932da28627c6892c400e039f073ba7ac19fb
Angel Jimenez <ajimenezcruz184@gmail.com>
2024-08-14 10:08:19 -0600

    reciduo de una division

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -5,29 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace miPrimerProyecto
-{
+{ 
     internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //obtener la secuencia fibonacci
-            int a = 0, b = 1, c = 1;
-            /*
-            0+1=1
-            1+0=1
-            1+1=2
-            2+1=3
-            3+2=5
-            */
-            while (c < 100)
-            {
-                Console.WriteLine("{0}+{1}={2}", a, b, c);
-                b = a;
-                a = c;
-                c = a + b;
-
-            }
-            Console.ReadLine();
+        int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+        foreach (int num in serie) {
+            Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "par" : "impar");
         }
+        Console.ReadLine();
     }
 }
+}
+
